e4b506d3152dbeefc45f8d84ac7f67a564ab51dd
Jacqueline Chávez <jacquelinechavez301@gmail.com>
2023-05-06 15:59:49 -0600

    slider

--- resources/views/Menu/buyit.blade.php
+++ resources/views/Menu/buyit.blade.php
@@ -80,28 +80,38 @@
       </div>
   </div>
 </nav>
-</header>
-	 <!--luego puede ir el resto de la pagina -->
-    <!--Slider-->
-    
-    <div class="wrapper altura-a-b">
-        
-        <i id="left" class="fa-solid fa-angle-left"></i>
-        <div class="carousel">
-            <img src="img/slider/IMAGEN-1.jpg" alt="">
-            <img src="img/slider/IMAGEN-2.jpg" alt="">
-            <img src="img/slider/IMAGEN-3.jpg" alt="">
-            <img src="img/slider/IMAGEN-4.jpg" alt="">
-            <img src="img/slider/IMAGEN-5.jpg" alt="">
-            <img src="img/slider/IMAGEN-6.jpg" alt="">
-            <img src="img/slider/IMAGEN-7.jpg" alt="">
-            <img src="img/slider/IMAGEN-8.jpg" alt="">
-            <img src="img/slider/IMAGEN-9.jpg" alt="">                 
-        </div>
-        <i id="right" class="fa-solid fa-angle-right"></i>
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class=" forma carousel-inner">
+    <div class="  carousel-item active">
+      <img src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg" class=" w-100" alt="...">
     </div>
-    <!--End Slider-->
-    </header>
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2019/10/11/12/23/hair-4541747_1280.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2019/10/11/12/29/hair-4541766_1280.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+
     
     <Main>
     <!--BUIYT - CARDS -->
