964b104ecda695b83deeb8043d1e42809b4e6de5
Ander503 <andersonmelendez73@gmail.com>
2023-05-07 11:25:46 -0600

    Agregando css y agregando estilo a oferta.blade.php

--- resources/views/oferta/templateOferta.blade.php
+++ resources/views/oferta/templateOferta.blade.php
@@ -8,11 +8,15 @@
     <title>BUYIT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel='stylesheet' href='https://getuikit.com/assets/uikit/dist/css/uikit.css?nc=2479'>
-    <!--Propio-->
-    <link rel="stylesheet" href="{{asset('css/style_1.css')}} ">
-    <link rel="stylesheet" href="{{asset('css/drop.css')}}">
-    <!--Slider-->
-    <link rel="stylesheet" href="{{asset('css/style_2_s.css')}}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <!--Propio-->       
+    <link rel="stylesheet" href="{{asset('css/oferta_style.css')}}">
+    
+
   </head>
   <body>
     <header>
