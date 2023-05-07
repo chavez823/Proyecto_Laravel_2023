f49231350f27670941bbde64ef750a34f52006a0
Ander503 <andersonmelendez73@gmail.com>
2023-05-06 22:09:17 -0600

    Arreglando diseño cambio contraseña

--- resources/views/Usuario/cambiodecontraseña.blade.php
+++ resources/views/Usuario/cambiodecontraseña.blade.php
@@ -5,21 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cambio de Contraseña</title>
-    <link rel="stylesheet" href="css/style_login.css">
-    <!--link rel="stylesheet" href="css/style_error.css"-->
+    
     <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&family=Poiret+One&display=swap" rel="stylesheet"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <!--nose de que son link-->
-
+    
 	  <!--ICONO-->
-	  <link rel="shortcut icon" href="img/icono.ico" type="image/x-icon">
+	  <link rel="shortcut icon" href="{{asset('img/icono.ico')}}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BUYIT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!--Propio-->
-    <link rel="stylesheet" href="css/style_1.css">
-    <link rel="stylesheet" href="css/categories.css">
+    <link rel="stylesheet" href="{{asset('css/style_1.css')}}">
+    <link rel="stylesheet" href="{{asset('css/categories.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style_login.css')}}">
 
 </head>
 <body>
