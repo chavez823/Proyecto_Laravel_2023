e297858762e4523ce6d3456368892608b73c1c31
Ander503 <andersonmelendez73@gmail.com>
2023-05-07 14:38:56 -0600

    Arreglando diseño Ofertas

--- resources/views/oferta/oferta.blade.php
+++ resources/views/oferta/oferta.blade.php
@@ -1,72 +1,79 @@
+<header>
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&family=Poiret+One&display=swap" rel="stylesheet">
+    
+    
+    <!--Propio-->       
+    <link rel="stylesheet" href="{{asset('css/oferta_style.css')}}">
+
+</header>
 @extends('oferta.templateOferta')
 
 @section('content')
 <?php
 	$fechaActual = date('Y-m-d');
 ?>
-<br>
-<br>
-<br>
-<br>
-<div class="container">
-	<form>
-	<fieldset disabled>                                                     
 
-                    <span class="login-form-title">OFERTA ENVIADA</span> 
-                          		
-                    <div class="container_c">                                       
-                        <div class="column_1">
-                            <!-- Nombre --> 
-                            <div class="wrap-input100"> 
-                                <input class="input100" type="text" name="titulo" placeholder="Titulo" value="{{$oferta[0]->Titulo}}" >	 
-                                <span class="focus-efecto"></span> 
-                            </div>
-
-                            <!-- Categoria --> 
-                            <div class="wrap-input100"> 
-                                <input class="input100" type="text" name="categoria" placeholder="categoria" value="{{$oferta[0]->Categoria}}" >	 
-                                <span class="focus-efecto"></span> 
-                            </div> 
-                           
-                            
-                            <!--Fecha fin-->
-                            <div class="wrap-input100"> 
-                                <input class="input100" type="text" name="fechafin" placeholder="Fecha Fin" value="{{$oferta[0]->FechaFin}}">	 
-                                <span class="focus-efecto"></span> 
-                            </div>
-                            
-                            
-                            
-                        </div>		
-
-                        <div class="column_2">
-                            <!-- Descripcion --> 
-                            <div class="wrap-input100"> 
-                                <input class="input100" type="text" name="descripcion" placeholder="Descripcion" value="{{$oferta[0]->Descripcion}}"> 
-                                <span class="focus-efecto"></span> 
-                            </div>
-
-                            <!--Fecha inicio-->
-                            <div class="wrap-input100"> 
-                                <input class="input100" type="text" name="fechainicio" placeholder="Fecha inicio" value="{{$oferta[0]->FechaInicio}}" >	 
-                                <span class="focus-efecto"></span> 
-                            </div>
-
-                            
-
-                        </div>
-                    </div> 
-		</fieldset>
-	</form>
-		
-		<form action="/Empresa/Estado/<?=$oferta[0]->ID_Oferta?>&<?php echo (strtotime($fechaActual)<strtotime($oferta[0]->FechaInicio)?2:3) ?>" method="post">
+<div class="container-login">
+    
+        <form action="/Empresa/Estado/<?=$oferta[0]->ID_Oferta?>&<?php echo (strtotime($fechaActual)<strtotime($oferta[0]->FechaInicio)?2:3) ?>" method="post">
 			@csrf
 			@method('PUT')
-	  	<button type="submit" class="btn btn-success">
-	   		Aceptar
-	  	</button>
-		</form>
-		<a href="#delete<?=$oferta[0]->ID_Oferta?>" data-toggle="modal" class="btn btn-danger">Rechazar</a>
+            
+    <div class="wrap-login">  
+        <fieldset>  
+        <span class="login-form-title">OFERTA ENVIADA</span>                                                                           
+                <div class="container_c">                                                           
+                    <div class="column_1">
+                        <!-- Nombre --> 
+                        <div class="wrap-input100"> 
+                            <label>Titulo</label>
+                            <input class="input100" type="text" name="titulo" placeholder="Titulo" value="{{$oferta[0]->Titulo}}" disabled>	 
+                            <span class="focus-efecto"></span> 
+                        </div>
+
+                        <!-- Categoria --> 
+                        <div class="wrap-input100">
+                            <label>Categoria</label> 
+                            <input class="input100" type="text" name="categoria" placeholder="categoria" value="{{$oferta[0]->Categoria}}" disabled>	 
+                            <span class="focus-efecto"></span> 
+                        </div> 
+                                                            
+                        <!--Fecha fin-->
+                        <div class="wrap-input100"> 
+                            <label>Fecha Fin</label>
+                            <input class="input100" type="text" name="fechafin" placeholder="Fecha Fin" value="{{$oferta[0]->FechaFin}}" disabled>	 
+                            <span class="focus-efecto"></span> 
+                        </div>   
+                        <button type="submit" class="btn btn-success">
+	   		                Aceptar
+	  	                </button>        
+                          <a href="#delete<?=$oferta[0]->ID_Oferta?>" data-toggle="modal" class="btn btn-danger" aria-disabled="false">Rechazar</a>                                                                                    
+                    </div>		
+
+                    <div class="column_2">
+                        <!-- Descripcion --> 
+                        <div class="wrap-input100"> 
+                            <label>Descripcion</label>
+                            <textarea class="input100" type="text" name="descripcion" placeholder="Descripcion" value="" disabled>{{$oferta[0]->Descripcion}}</textarea> 
+                            <span class="focus-efecto"></span> 
+                        </div>
+
+                        <!--Fecha inicio-->
+                        <div class="wrap-input100"> 
+                            <label>Fecha Inicio</label>
+                            <input class="input100" type="text" name="fechainicio" placeholder="Fecha inicio" value="{{$oferta[0]->FechaInicio}}" disabled>	 
+                            <span class="focus-efecto"></span> 
+                        </div>
+                         
+                    </div>
+                </div>  
+            </fieldset>                 		                 
+        </div>        
+	</form>
+		
+		
+	  	
 	</div>
  <!-- Inicio modal -->
  <div class="modal" id="delete<?=$oferta[0]->ID_Oferta?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
--- resources/views/oferta/templateOferta.blade.php
+++ resources/views/oferta/templateOferta.blade.php
@@ -9,9 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel='stylesheet' href='https://getuikit.com/assets/uikit/dist/css/uikit.css?nc=2479'>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&family=Poiret+One&display=swap" rel="stylesheet">
 
     <!--Propio-->       
     <link rel="stylesheet" href="{{asset('css/oferta_style.css')}}">
