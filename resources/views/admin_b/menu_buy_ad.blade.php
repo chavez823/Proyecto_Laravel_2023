6ecce09651138c681a7e1fd58c751bac993747ff
Ander503 <andersonmelendez73@gmail.com>
2023-05-09 21:05:17 -0600

    Arreglando

--- resources/views/admin_b/menu_buy_ad.blade.php
+++ resources/views/admin_b/menu_buy_ad.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <div class="btn_back">
-        <a href="/Empresa"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-bar-left" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <a href="/buyit"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-bar-left" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <line x1="4" y1="12" x2="14" y2="12" />
             <line x1="4" y1="12" x2="8" y2="16" />
