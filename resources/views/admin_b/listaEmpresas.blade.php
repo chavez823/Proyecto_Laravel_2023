9bdc70749ae2d7dd651111bc39329f5fd512342f
Ander503 <andersonmelendez73@gmail.com>
2023-05-09 20:58:15 -0600

    Agregando boton volver

--- resources/views/admin_b/listaEmpresas.blade.php
+++ resources/views/admin_b/listaEmpresas.blade.php
@@ -16,8 +16,18 @@
 
   </head>
   <body >
+    
     <div class="data">
     <h2><strong>Empresas</strong></h2>
+    <div class="btn_back">
+        <a href="/Empresa"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-bar-left" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="4" y1="12" x2="14" y2="12" />
+            <line x1="4" y1="12" x2="8" y2="16" />
+            <line x1="4" y1="12" x2="8" y2="8" />
+            <line x1="20" y1="4" x2="20" y2="20" />
+          </svg></a>
+    </div>
     <center>
     <div class="col-sm-11 col-sm-offset-1">
     <table class="uk-table uk-table-divider uk-table-hover">
